d7c8edf3c2ed719d38ff94b3ff8571cdcd24f223
Kawauso <161272652+Ishikishik@users.noreply.github.com>
2025-10-03 21:06:03 +0900

    hatuonn関数を作成

--- main/library.hpp
+++ main/library.hpp
@@ -1,2 +1,24 @@
-#include "library.hpp"
+#ifndef MY_LSI_H
+#define MY_LSI_H
 
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+class LSI {
+public:
+    // コンストラクタでSoftwareSerialインスタンスを渡す
+    LSI(SoftwareSerial& serial);
+
+    // 初期化
+    void begin(long baudrate = 9600);
+
+    // 発声させる
+    void hatuonn(String msg);
+
+private:
+    SoftwareSerial& mySerial;
+
+    void sendToLSI(String s);
+};
+
+#endif
--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,20 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include "library.hpp"
+
+// SoftwareSerialのインスタンス作成
+SoftwareSerial mySerial(0, 1); // 実際のピン番号はライブラリ外で管理
+LSI lsi(mySerial);
 
 void setup() {
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println("LSI test start");
+
+    lsi.begin(9600);
 }
 
 void loop() {
+    lsi.hatuonn("chidorinochi/yoshinonoyo/uenonou/fujisannofu/choofu");
+    delay(3000);
 }
